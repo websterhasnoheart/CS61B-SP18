bd97be87c471659252d0b7cd5a83eedda3ab8b9b
zhijiangchen <websterchan1998@gmail.com>
2021-08-02 22:43:21 +0800

    update proj1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -17,6 +17,6 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'a'));
         assertTrue(offByOne.equalChars('%', '&'));
-        assertTrue(offByOne.equalChars('A','b'));
+        assertTrue(offByOne.equalChars('A', 'b'));
     }
 }
