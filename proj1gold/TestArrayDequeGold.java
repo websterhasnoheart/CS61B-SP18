9a710ff17352be553c8b74b990dc48f3c4915fe5
zhijiangchen <websterchan1998@gmail.com>
2021-08-03 10:54:16 +0800

    bug fixed

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -3,52 +3,53 @@ import org.junit.Test;
 
 public class TestArrayDequeGold {
     @Test
-    public void test() {
-        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
-        String msg = "";
-        int s = 0;
+    public void testArrayDeque() {
+        ArrayDequeSolution<Integer> rdeque = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> wdeque = new StudentArrayDeque<>();
+        /* Store message in a StringBuilder() */
+        StringBuilder msg = new StringBuilder();
+        int size = 0;
 
-        for (int i = 0; i < 1000; ++i) {
+        for (int i = 0; i < 1000; i++) {
             if (i % 5 == 0) {
-                msg += "size()\n";
-                assertEquals(msg, ads.size(), sad.size());
+                msg.append("size()\n");
+                assertEquals(msg.toString(), rdeque.size(), wdeque.size());
             }
-            double rand = StdRandom.uniform();
-            if (rand < 0.25) {
-                sad.addFirst(i);
-                ads.addFirst(i);
-                msg += "addFirst(" + i + ")\n";
-                s++;
-                assertEquals(msg, ads.get(0), sad.get(0));
-            } else if (rand < 0.5) {
-                sad.addLast(i);
-                ads.addLast(i);
-                msg += "addLast(" + i + ")\n";
-                s++;
-                assertEquals(msg, ads.get(s - 1), sad.get(s - 1));
-            } else if (rand < 0.75) {
-                if (ads.isEmpty()) {
-                    msg += "isEmpty()\n";
-                    assertTrue(msg, sad.isEmpty());
+            double random = StdRandom.uniform();
+            if (random < 0.25) {
+                rdeque.addFirst(i);
+                wdeque.addFirst(i);
+                size++;
+                msg.append("addFirst(" + i + ")\n");
+                assertEquals(msg.toString(),rdeque.get(0),wdeque.get(0));
+            } else if (random < 0.5 ) {
+                rdeque.addLast(i);
+                wdeque.addLast(i);
+                size++;
+                msg.append("addLast(" + i + ")\n");
+                assertEquals(msg.toString(), rdeque.get(size-1), wdeque.get(size-1));
+            } else if (random < 0.75) {
+                if (rdeque.isEmpty()) {
+                    msg.append("isEmpty()\n");
+                    assertTrue(msg.toString(), wdeque.isEmpty());
                     continue;
                 }
-                Integer act = sad.removeFirst();
-                Integer exp = ads.removeFirst();
-                msg += "removeFirst()\n";
-                s--;
-                assertEquals(msg, exp, act);
+                Integer right = rdeque.removeFirst();
+                Integer wrong = wdeque.removeFirst();
+                size--;
+                msg.append("removeFirst()\n");
+                assertEquals(msg.toString(),right, wrong);
             } else {
-                if (ads.isEmpty()) {
-                    msg += "isEmpty()\n";
-                    assertTrue(msg, sad.isEmpty());
+                if (rdeque.isEmpty()) {
+                    msg.append("isEmpty()");
+                    assertTrue(msg.toString(), wdeque.isEmpty());
                     continue;
                 }
-                Integer act = sad.removeLast();
-                Integer exp = ads.removeLast();
-                msg += "removeLast()\n";
-                s--;
-                assertEquals(msg, exp, act);
+                Integer right = rdeque.removeLast();
+                Integer wrong = wdeque.removeLast();
+                size--;
+                msg.append("removeLast()\n");
+                assertEquals(msg.toString(), right, wrong);
             }
         }
     }
